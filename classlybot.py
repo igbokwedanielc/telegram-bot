3a0d29e9cf5d88b0128a3829702367750ec2a903
igbokwedanielc <igbokwedanielc@gmail.com>
2025-12-05 21:48:22 +0100

    Updated classlybot.py to use environment variable for BOT_TOKEN

--- classlybot.py
+++ classlybot.py
@@ -1,10 +1,10 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import os
 import logging
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 logging.basicConfig(level=logging.INFO)
 
-# Replace with your bot token
-TOKEN = "8557354376:AAHfNEW2CnQRfZ9u3zDKzI1M4n-p2eKflTQ"
+TOKEN = os.getenv("BOT_TOKEN")  # Render will inject this
 
 async def start(update, context):
     await update.message.reply_text("Hello! The bot is now active 😊")
@@ -15,10 +15,8 @@ async def reply(update, context):
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-
     app.run_polling()
 
 if __name__ == "__main__":
